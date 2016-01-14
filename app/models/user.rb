d7935b5381509c01dd917dd4cf602bf4a49d37c5
jdbwin <dbwin7@gmail.com>
2016-01-14 00:02:40 -0500

    added bookshelf

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
 
+  def posting_trades
+    Trade.where(posting_user: id)
+  end
+
+  def receiving_trades
+    Trade.where(posting_user: id)
+  end
+
 end
