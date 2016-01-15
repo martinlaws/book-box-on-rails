d3fa3b7075d3253199da1ae8e39a9da543886f2a
jdbwin <dbwin7@gmail.com>
2016-01-15 13:45:03 -0500

    fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
   def create
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      redirect_to trade_wall_index_path
     else
       flash.now[:alert] = "Log in failed ..."
       render :new
