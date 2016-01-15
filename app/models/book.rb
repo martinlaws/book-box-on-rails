4ca16277a74e8cf795d04d30b5da8ab7be6d0194
jdbwin <dbwin7@gmail.com>
2016-01-14 23:04:07 -0500

    added availability option on book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,20 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_many :trades
 
+  def not_owned_by_current_user?(current_user)
+    user_id != current_user.id
+  end
+
+  def available?
+    self.availability == true
+  end
+
+  def make_available
+    self.availability = true
+  end
+
+  def make_unavailable
+    self.availability = false
+  end
+
 end
