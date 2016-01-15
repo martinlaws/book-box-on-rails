804ac9549ccac9d6f1c1cc3bad7e3e2eba149646
jdbwin <dbwin7@gmail.com>
2016-01-15 16:11:13 -0500

    added "pending trade" notification on book when pending trade

--- app/assets/javascripts/trade_wall.js
+++ app/assets/javascripts/trade_wall.js
@@ -1,11 +1,14 @@
 
 var loaded = function() {
   $('.trade-button').click(function() {
-    this.value = 'Pending trade...';
+    var $this = $(this);
+
+    setTimeout(function() {
+      $this.replaceWith("<div>Pending trade ...</div>")
+    }, 100);
   });
 };
 
 //Reg syntax for page reload, turbolinks syntax for routes
 $(document).ready(loaded);
 $(document).on('page:load', loaded);
-
