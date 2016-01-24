faad75c43af786803c70e06bdee76006a3a41b14
jdbwin <dbwin7@gmail.com>
2016-01-23 23:11:02 -0500

    optimised some db calls for bookshelf

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -2,6 +2,8 @@ class BookshelfController < ApplicationController
   before_action :require_login
 
   def index
-    @books = Books.where(:user_id == current_user.id)
+    @user_books = Book.where(:user_id == current_user)
+    @posting_trades = current_user.posting_trades.includes(:book)
+    @receiving_trades = current_user.receiving_trades.includes(:book)
   end
 end
